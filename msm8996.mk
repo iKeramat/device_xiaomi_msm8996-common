be025d123c8b8eb0ea66cf1a52cb630eb9423f98
iKeramat <iKeramat80@gmail.com>
2019-08-14 12:23:16 +0430

    msm8996-common:: extra: ringtone: Add Mi ringtone
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat80@gmail.com>
    Change-Id: Ibcd60d133ba7416a934f6c2f4d078363b782a644

--- msm8996.mk
+++ msm8996.mk
@@ -271,6 +271,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
 
+# Mi Ringtone
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/extra/ringtone/Mi.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/Mi.ogg
+	
 # Net
 PRODUCT_PACKAGES += \
     android.system.net.netd@1.0 \
