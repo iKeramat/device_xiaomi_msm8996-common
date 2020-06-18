ecab91a86b889c60d468911a124c8b163b215253
iKeramat <ikeramat80@gmail.com>
2020-06-18 23:11:26 +0430

    msm8996-common: Move init.spectrum.sh to TARGET_OUT_VENDOR_EXECUTABLES
    Signed-off-by: iKeramat <ikeramat80@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -23,7 +23,7 @@ LOCAL_MODULE		:= init.spectrum.sh
 LOCAL_MODULE_TAGS	:= optional
 LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= etc/init.spectrum.sh
-LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH	:= $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT) 
 
 # Common init scripts
