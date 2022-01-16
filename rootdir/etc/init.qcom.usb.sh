18586d2e4da80d102fb51ef1129a94803d19a3db
Mohammad Hasan Keramat J <ikeramat@protonmail.com>
2022-01-16 12:58:21 +0330

    msm8996-common: rootdir: init.qcom.usb.sh: Remove useless strings from usb product string
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>

--- rootdir/etc/init.qcom.usb.sh
+++ rootdir/etc/init.qcom.usb.sh
@@ -140,10 +140,8 @@ esac
 # check configfs is mounted or not
 if [ -d /config/usb_gadget ]; then
 	# Chip-serial is used for unique MSM identification in Product string
-	msm_serial=`cat /sys/devices/soc0/serial_number`;
-	msm_serial_hex=`printf %08X $msm_serial`
 	machine_type=`cat /sys/devices/soc0/machine`
-	setprop vendor.usb.product_string "$machine_type-$soc_hwplatform _SN:$msm_serial_hex"
+	setprop vendor.usb.product_string "$machine_type-$soc_hwplatform"
 
 	# ADB requires valid iSerialNumber; if ro.serialno is missing, use dummy
 	serialnumber=`cat /config/usb_gadget/g1/strings/0x409/serialnumber 2> /dev/null`
