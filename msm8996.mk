f96ca642d84142c92c71e03bb21dab70c6fa60c3
Mohammad Hasan Keramat J <ikeramat@protonmail.com>
2022-09-18 19:56:50 +0430

    switch to qti usb
    Change-Id: I1a81c258a7497a77b724b55f49651b906b3e599b

--- msm8996.mk
+++ msm8996.mk
@@ -153,11 +153,9 @@ PRODUCT_PACKAGES += \
 # Common init scripts
 PRODUCT_PACKAGES += \
     fstab.qcom \
-    init.msm.usb.configfs.rc \
     init.qcom.rc \
     init.qcom.sh \
     init.qcom.power.rc \
-    init.qcom.usb.rc \
     init.recovery.qcom.rc \
     ueventd.qcom.rc
 
@@ -404,9 +402,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
 
-# USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service
+    android.hardware.usb@1.3-service-qti \
+    init.qcom.usb.rc \
+    init.qcom.usb.sh
+
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/qcom/opensource/usb/etc
 
 # VNDK
 PRODUCT_PACKAGES += \
