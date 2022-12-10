LineageOS/android_device_xiaomi_msm8996-common

msm8996-common: Add a check before adding dependencies
Without a check, when performing a vendor blob regen it'll add another
dependency on top of any existing one

Change-Id: I9c7f17082fd850235c794d44cec3cda0c4b883ef

extract-files.sh
@@ -43,19 +43,25 @@ function blob_fixup() {
         sed -i "s/libhidltransport.so/libcutils-v29.so\x00\x00\x00/" "${2}"
         ;;
     vendor/bin/imsrcsd)
-        "${PATCHELF}" --add-needed "libbase_shim.so" "${2}"
+        for LIBBASE_SHIM in $(grep -L "libbase_shim.so" "${2}"); do
+            "${PATCHELF}" --add-needed "libbase_shim.so" "$LIBBASE_SHIM"
+        done
         ;;
     vendor/bin/slim_daemon)
         "${PATCHELF}" --replace-needed "android.frameworks.sensorservice@1.0.so" "android.frameworks.sensorservice@1.0-v27.so" "${2}"
         ;;
     vendor/lib64/hw/android.hardware.bluetooth@1.0-impl-qti.so)
-        "${PATCHELF}" --add-needed "libbase_shim.so" "${2}"
+        for LIBBASE_SHIM in $(grep -L "libbase_shim.so" "${2}"); do
+            "${PATCHELF}" --add-needed "libbase_shim.so" "$LIBBASE_SHIM"
+        done
         ;;
     vendor/lib64/hw/vulkan.msm8996.so)
         sed -i "s/vulkan.msm8953.so/vulkan.msm8996.so/g" "${2}"
         ;;
     vendor/lib64/lib-uceservice.so)
-        "${PATCHELF}" --add-needed "libbase_shim.so" "${2}"
+        for LIBBASE_SHIM in $(grep -L "libbase_shim.so" "${2}"); do
+            "${PATCHELF}" --add-needed "libbase_shim.so" "$LIBBASE_SHIM"
+        done
         ;;
     vendor/lib64/libsettings.so)
         "${PATCHELF}" --replace-needed "libprotobuf-cpp-full.so" "libprotobuf-cpp-full-v28.so" "${2}"

iKeramat/device_xiaomi_msm8996-common

Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>

extract-files.sh
@@ -40,13 +40,19 @@ function blob_fixup() {
         sed -i "s/name=\"android.hidl.manager-V1.0-java/name=\"android.hidl.manager@1.0-java/g" "${2}"
         ;;
     system_ext/lib64/lib-imscamera.so)
-        "${PATCHELF}" --add-needed "libgui_shim.so" "${2}"
+        for LIBGUI_SHIM in $(grep -L "libgui_shim.so" "${2}"); do
+            "${PATCHELF}" --add-needed "libgui_shim.so" "${LIBGUI_SHIM}"
+        done
         ;;
     system_ext/lib64/lib-imsvideocodec.so)
-        "${PATCHELF}" --add-needed "libui_shim.so" "${2}"
+        for LIBUI_SHIM in $(grep -L "libui_shim.so" "${2}"); do
+            "${PATCHELF}" --add-needed "libui_shim.so" "${LIBUI_SHIM}"
+        done
         ;;
     system_ext/lib64/lib-imsvt.so)
-        "${PATCHELF}" --add-needed "libgui_shim.so" "${2}"
+        for LIBGUI_SHIM in $(grep -L "libgui_shim.so" "${2}"); do
+            "${PATCHELF}" --add-needed "libgui_shim.so" "${LIBGUI_SHIM}"
+        done
         ;;
     system_ext/lib64/libdpmframework.so)
         sed -i "s/libhidltransport.so/libcutils-v29.so\x00\x00\x00/" "${2}"